76df6ad3a70c9448dbd8a0787b0c61a19a8fa85e
baitcode <batiyiv@gmail.com>
2020-01-21 23:39:19 +0000

    Updates

---
+++ precache-manifest.199c9b0bc00b28648bb0f1d684465996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae3535760d9ff80ff83dc4d3d110da1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693de70db73f30f996f1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cee09796a263df8edba2",
+    "url": "/static/js/2.e2243fe3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e2243fe3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "693de70db73f30f996f1",
+    "url": "/static/js/main.88596f3b.chunk.js"
+  },
+  {
+    "revision": "5aad247fc37c2fb4f30a",
+    "url": "/static/js/runtime-main.6f92c6a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

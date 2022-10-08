cccf651912e7b8670593182e8b62a4a72396be7a
ekaterina.loshchinina@gmail.com <katerinaloschinina@Katerinas-MacBook-Air.local>
2022-10-08 12:57:55 +0300

    more tweaks

--- src/Video.js
+++ src/Video.js
@@ -1,15 +1,45 @@
+import { useState, useEffect } from 'react';
+
 import video_mp4 from './576p.mp4';
 import video_webm from './576p.mp4';
+import poster from './screen.png'
 
 import './Video.css';
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
 function Video() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className="ko-video">
-      <video autoPlay muted loop playsinline poster="video.png">
-        <source src={video_webm} type="video/webm"/>
-        <source src={video_mp4} type="video/mp4"/>
-      </video>
+      {windowDimensions > '800px' ? (
+        <video autoPlay muted loop autobuffer playsinline poster={poster}>
+          <source src={video_webm} type="video/webm"/>
+          <source src={video_mp4} type="video/mp4"/>
+        </video>
+      ) : (
+        <video controls muted loop autobuffer playsinline poster={poster}>
+          <source src={video_webm} type="video/webm"/>
+          <source src={video_mp4} type="video/mp4"/>
+        </video>
+      )}
+      
     </section>
   );
 }

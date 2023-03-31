1cff44b7ac6026b7721ae1091781f421b725ceed
ekaterina.loshchinina@gmail.com <katerinaloschinina@192.168.0.14>
2023-03-31 02:04:25 +0100

    WIP of kot

---
+++ src/Yokai.js
@@ -0,0 +1,22 @@
+import Header from './Header';
+import Logo from './Logo';
+import Video from './Video';
+import Divider from './Divider';
+import Gallery from './Gallery';
+import About from './About';
+import YokaiAbout from './YokaiAbout';
+import Team from './Team';
+import Footer from './Footer';
+
+import './Kotyatki.css';
+
+function Kotyatki() {
+  return (
+    <>
+      <Header></Header>
+      <YokaiAbout></YokaiAbout>
+    </>
+  );
+}
+
+export default Kotyatki;
---
+++ src/YokaiAbout.js
@@ -0,0 +1,42 @@
+import './YokaiAbout.css';
+
+function YokaiAbout() {
+  const link = "https://www.youtube.com/embed/imKELSX2kw8"
+
+  return (
+    <>
+    <section className="yo-about" id="about">
+      <div className="about">
+        <p>Help us build an oasis of audio visual tactile pleasure.
+          A place to explore the playful connection and curiosity through tension and touch.
+          A tactile multi-sensory interactive sculpture. Gigantic Yokai hidden deep in the playa.</p>
+        <p>The Kinky Yokai is a multi-dimensional portal inviting participants to explore
+          self-discovery, honesty, and creativity through its multi-sensory features. This tactile,
+          interactive sculpture lets attendees delve into the relationship between confinement and
+          curiosity through tension and touch. We aim to create an immersive experience, allowing
+          individuals to shed societal labels and embrace their true selves in a safe environment.
+          The Kinky Yokai is a sanctuary for curiosity and self-expression. Engage with its enigmatic
+          allure and connect with fellow explorers. Experience shared play with a visual musical instrument,
+          creating a kinesthetic symphony and connecting with the environment through interactive sandplay
+          and visuals.
+          Pull it, play it, enjoy it, and become one with the Kinky Yokai. Let this mystical
+          creation anchor you in the present moment, savouring the surreal atmosphere crafted to blur
+          the lines between realities.
+        </p>
+      </div>
+      </section>
+      <section>
+        <div className="video-yokai">
+          <iframe
+            src={link}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube" />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default YokaiAbout;
